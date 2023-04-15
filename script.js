5319848d16ec0be06ea5b4b95ca267d8a7781e40
leticea <nascimento.leticeah@gmail.com>
2023-04-15 10:57:20 -0300

    feat(script.js): main js codes

---
+++ script.js
@@ -0,0 +1,4 @@
+const colorOne = document.getElementById("color-a");
+const colorTwo = document.getElementById("color-b");
+const outputCode = document.getElementById("code");
+const currentDirection = "to bottom";
