09a3030e183fc9dbe7621368029cd0a133248523
leticea <nascimento.leticeah@gmail.com>
2023-04-15 11:00:17 -0300

    feat(script.js): setDirection function

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const colorOne = document.getElementById("color-a");
 const colorTwo = document.getElementById("color-b");
 const outputCode = document.getElementById("code");
 const currentDirection = "to bottom";
+
+function setDirection(value, _this) {
+  let directions = document.querySelectorAll(".buttons button");
+  for (let i of directions) {
+    i.classList.remove("active");
+  }
+
+  _this.classList.add("active");
+  currentDirection = value;
+}
